a9dd8af27b233ffba51fae6f7ab7f09523f3f5e9
Juan David Abril <124622312+Juan-Abril21@users.noreply.github.com>
2024-09-27 21:06:09 -0700

    Update CuentaDTO.java

--- src/main/java/com/banco/arquitectura/controller/dto/CuentaDTO.java
+++ src/main/java/com/banco/arquitectura/controller/dto/CuentaDTO.java
@@ -3,5 +3,4 @@ package com.banco.arquitectura.controller.dto;
 import java.time.LocalDate;
 
 public record CuentaDTO(Long id, String cedula, String nombre, Double saldo, LocalDate fechaCreacion) {
-
 }
