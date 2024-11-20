dcd889911c3c2c16a38d958592e3eb1e4bbdaa68
Juan Abril <jdac149@gmail.com>
2024-11-20 09:55:23 -0500

    Update CORS for vite

--- src/main/java/com/banco/arquitectura/config/WebConfig.java
+++ src/main/java/com/banco/arquitectura/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://bancofrontend.vercel.app","http://localhost:3000")
+            .allowedOrigins("https://bancofrontend.vercel.app","http://localhost:3000","http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
